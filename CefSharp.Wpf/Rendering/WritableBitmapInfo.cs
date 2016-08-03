98c529f4e24c306d8dd1863ed39637a0e61b367c
Antonyo P <Antonyo P>
2016-08-03 10:20:54 +0900

    Replace GC ollection from the bitmap to the wpf browser to reuse existing code and affect any kind of bitmap.

--- CefSharp.Wpf/Rendering/WritableBitmapInfo.cs
+++ CefSharp.Wpf/Rendering/WritableBitmapInfo.cs
@@ -73,9 +73,6 @@ namespace CefSharp.Wpf.Rendering
         public override void ClearBitmap()
         {
             Bitmap = null;
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-            GC.Collect();
         }
 
         /// <summary>
